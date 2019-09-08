b8042898709ee54f951c2fdaffc1f93da2f75afd
Ariel <arielord@buffalo.edu>
2019-09-08 23:39:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(name){
+  let driverAppended = drivers.slice(0,drivers.length,name);
+  return driverAppended;
+}
